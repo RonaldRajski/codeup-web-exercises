c6bbf86077f25e2eb262bd8cce0a8c0a271dd488
Ronald Rajski <ronaldrajski@gmail.com>
2020-11-05 16:51:40 -0600

    Becoming a JS whiz

---
+++ js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("Hello from the js console");
+console.log("hello from  the other side");
+alert("You just won a new iPad");
+
+// the following line will show the OK/CANCEL confirm dialog
+var confirmed = confirm('Are you sure you want to Skydive?');
+console.log(confirmed); // will be either true or false
+
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);

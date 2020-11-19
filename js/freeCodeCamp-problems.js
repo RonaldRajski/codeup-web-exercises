afa709863be36efdc23381d2e388579c1cd72949
Ronald Rajski <ronaldrajski@gmail.com>
2020-11-19 09:44:26 -0600

    Welcome update

--- js/freeCodeCamp-problems.js
+++ js/freeCodeCamp-problems.js
@@ -1,4 +1,4 @@
-// 80
+// 83
 //Setup
 var myPlants = [
     {
@@ -23,7 +23,44 @@ var secondTree = myPlants[1].list[1];
 var thirdFlower = myPlants[0].list[2];
 var secondFlower = myPlants[0].list[1]
 
-console.log(secondTree);
+console.log(secondTree);    //pine
 console.log(thirdFlower);  //dandelion
 console.log(secondFlower); //tulip
+
+// 84 Record collection
+//setup
+var collection = {
+    2548: {
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let it Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999','Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439:{
+        albumTitle: 'ABBA Gold'
+    }
+};
+function updateRecords(object, id, prop, value) {
+    if (prop !== 'tracks' && value !== "") {
+        object[id][prop] = value;
+    } else if (prop === "tracks" && !object[id].hasOwnProperty("tracks")) {
+        object[id][prop] = [value];
+    } else if (prop === "tracks" && value !== "") {
+        object[id][prop].push(value);
+    } else if (value === "") {
+        delete object[id][prop];
+    }
+    return object;
+}
+
+console.log(updateRecords(collection, 5439, 'artist', 'ABBA'));
+
 

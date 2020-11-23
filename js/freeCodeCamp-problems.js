392af56da300b47ff0e456c882b1e28b2ac4dbab
Ronald Rajski <ronaldrajski@gmail.com>
2020-11-23 14:51:35 -0600

    FreeCodeCamp

--- js/freeCodeCamp-problems.js
+++ js/freeCodeCamp-problems.js
@@ -63,4 +63,50 @@ function updateRecords(object, id, prop, value) {
 
 console.log(updateRecords(collection, 5439, 'artist', 'ABBA'));
 
+// 83
+// Setup
+var myArray = [];
+
+// Only change code below this line
+var i = 5;
+while (i > -1){
+    myArray.push(i);
+    i--;
+}
+// 84 For Loop
+// Setup
+var myArray = [];
+
+// Only change code below this line
+for(var i =1; i<6; i++){
+    myArray.push(i);
+}
+// 85
+// Setup
+var myArray = [];
+
+// Only change code below this line
+// increment odd numbers
+for(var i = 1; i < 10; i+=2){
+    myArray.push(i);
+}
+// 86
+// Setup
+var myArray = [];
+
+// Only change code below this line
+// Counting down odds for loop
+for(var i = 9; i>0; i -=2){
+    myArray.push(i);
+}
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+
+// Only change code below this line
+// 87% complete
+// Find the total of the array
+var total = 0;
+for (var i = 0; i < myArr.length; i++ ){
+    total += myArr[i];
+}
 

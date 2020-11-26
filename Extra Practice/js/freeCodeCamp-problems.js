19b81ac82414eea270e68271160f751c393d0df2
Ronald Rajski <ronaldrajski@gmail.com>
2020-11-25 22:05:37 -0600

    Free code camp exercise

--- Extra Practice/js/freeCodeCamp-problems.js
+++ Extra Practice/js/freeCodeCamp-problems.js
@@ -148,5 +148,43 @@ function sum(arr, n) {
         return sum(arr, n - 1) + arr[n - 1];
     }
 
+
     // Only change code above this line
 }
+
+// Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop){
+// Only change code below this line
+
+// Only change code above this line
+}
+
+lookUpProfile("Akira", "likes");

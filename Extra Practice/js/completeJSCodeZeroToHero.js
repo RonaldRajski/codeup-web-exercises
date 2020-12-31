b87156e488660447ffd39162080e5fded9af0342
Ronald Rajski <ronaldrajski@gmail.com>
2020-12-30 18:47:01 -0600

    completed code challenge 3 JS Zero to Hero

--- Extra Practice/js/completeJSCodeZeroToHero.js
+++ Extra Practice/js/completeJSCodeZeroToHero.js
@@ -152,6 +152,98 @@ else{
     console.log(`John's BMI (${BMIJohn1})  is higher than Mark's (${BMIMark1})`);
 }
 
+// conversion versus coercion
+
+const inputYear = '1991';
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+
+// type coercion
+
+console.log(('I am ' + 23 + 'years old'));
+console.log('23' - '10' - 3);
+
+
+const hasDriversLicense = true;
+const hasGoodVision =  true;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+
+// Coding Challenge #3
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each
+// other 3 times. The winner with the highest average score wins a trophy!
+//     Your tasks:
+//     1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+// team only wins if it has a higher score than the other team, and the same time a
+// score of at least 100 points. Hint: Use a logical operator to test for minimum
+//     score, as well as multiple else-if blocks �
+// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+// both teams have the same score and both have a score greater or equal 100
+// points. Otherwise, no team wins the trophy
+// Test data:
+//     § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+var scoreDolphins = (96 + 108 + 89)/3;
+var scoreKoalas = (88 + 91 + 110)/ 3;
+
+var scoreDolphins1 = (97 + 112 + 101)/3;
+var scoreKoalas1 = (109 + 95 + 123)/ 3;
+
+var scoreDolphins2 = (97 + 112 + 101)/3;
+var scoreKoalas2 = (109 + 95 + 106)/ 3;
+
+
+
+console.log(scoreDolphins, scoreKoalas);
+
+if(scoreDolphins > scoreKoalas){
+    console.log("Dolphins win the trophy!");
+}
+else if(scoreKoalas > scoreDolphins){
+    console.log("Koalas win the trophy");
+}
+else{
+    console.log("Tie game!");
+}
+
+if(scoreDolphins1 > scoreKoalas1 && scoreDolphins1 >= 100){
+    console.log("Dolphins win the trophy!");
+}
+else if(scoreKoalas1 > scoreDolphins1 && scoreKoalas1 >=100){
+    console.log("Koalas win the trophy");
+}else{
+    scoreDolphins1 === scoreKoalas1
+    console.log("Tie game!");
+}
+
+
+if(scoreDolphins2 > scoreKoalas2 && scoreDolphins2 >= 100){
+    console.log("Dolphins win the trophy!");
+}
+else if(scoreKoalas2 > scoreDolphins2 && scoreKoalas2 >=100){
+    console.log("Koalas win the trophy");
+}else if(scoreDolphins2 === scoreKoalas2 && scoreDolphins2 >=100 && scoreKoalas2 >=100){
+    console.log("Tie game!");
+}
+else {
+    console.log("No Winner!");
+
+}
+
+
+
+
 
 
 

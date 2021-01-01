84ecb243d4658d0413274844d5dde53bab4d06e1
Ronald Rajski <ronaldrajski@gmail.com>
2020-12-31 22:16:53 -0600

    code challenge 4 JS Zero to Hero

--- Extra Practice/js/completeJSCodeZeroToHero.js
+++ Extra Practice/js/completeJSCodeZeroToHero.js
@@ -271,6 +271,69 @@ switch (day) {
 
 }
 
+if (day === "Monday"){
+    console.log("Plan course structure");
+    console.log("Go to coding meetup");
+
+}
+else if (day === "Tuesday"){
+    console.log("Prepare theory videos");
+
+}
+else if (day === "Wednesday" || day === "Thursday"){
+    console.log("Write code");
+
+}
+else if (day === "Friday"){
+    console.log("Record videos");
+
+}
+
+else if (day === "Saturday" || day === "Sunday"){
+    console.log("Enjoy the weekend :D");
+
+}
+else{
+
+    console.log("Not a valid day");
+}
+
+const age = 23;
+
+// age >= 18 ? console.log("I like to drink wine") :
+// console.log("I like to drink water");
+
+const drink = age >="wine: water";
+console.log(drink);
+
+let drink2;
+if(age>=18){
+    drink2 = "wine";
+}
+else {
+    drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine" : "water"}`);
+
+// Coding Challenge #4
+// Steven wants to build a very simple tip calculator for whenever he goes eating in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+// Your tasks:
+//     1. Calculatethetip,dependingonthebillvalue.Createavariablecalled'tip'for this. It's not allowed to use an if/else statement 😅 (If it's easier for you, you can start with an if/else statement, and then try to convert it to a ternary operator!)
+//     2. Printastringtotheconsolecontainingthebillvalue,thetip,andthefinalvalue (bill + tip). Example: “The bill was 275, the tip was 41.25, and the total value 316.25”
+// Test data:
+//     § Data 1: Test for bill values 275, 40 and 430 Hints:
+//     § To calculate 20% of a value, simply multiply it by 20/100 = 0.2 § Value X is between 50 and 300, if it's>= 50 && <= 300
+
+
+let bill = 275;
+let tip = bill <= 300 && bill >= 50 ? bill * .15 :
+bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+
+
 
 
 

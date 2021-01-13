ad42d75b84efa35b2f2d0b706b4d9d5ae98dec03
Ronald Rajski <ronaldrajski@gmail.com>
2021-01-13 10:09:48 -0600

    completed map-filter-reduce

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -36,3 +36,62 @@ const users = [
     }
 ];
 
+// Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+
+
+let threeLanguages = users.filter(user => user.languages.length >=3);
+
+console.table(threeLanguages);
+
+
+// Use .map to create an array of strings where each element is a user's email address
+
+        let emails =[];
+
+        users.map(user => emails.push(user.email));
+
+        console.log(emails);
+        console.table(emails);
+
+// Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
+
+    let totalUsersExperience = users.reduce(((totalExperience, user) => totalExperience += user.yearsOfExperience), 0);
+
+    let averageUsersExperience = totalUsersExperience/ users.length;
+
+console.log(averageUsersExperience);
+
+// Use .reduce to get the longest email from the list of users.
+
+let longestEmail = "";
+    users.reduce(((letterCount, user) => {
+        if(user.email.length > letterCount){
+            letterCount = user.email.length;
+            longestEmail = user.email;
+        }
+        return letterCount
+    }), 0);
+
+console.log(longestEmail);
+
+// Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+
+let instructors = users.reduce(((str, user) => str += `${user.name}`), 'Your instructors are : ');
+
+console.log(instructors);
+
+// Use .reduce to get the unique list of languages from the list of users.
+
+
+let knownLanguages = users.reduce((languages, user) =>{
+    for(let language of user.languages){
+        if(!languages.includes(languages)){
+            languages.push(language)
+        }
+    }
+    return languages;
+}, []);
+
+console.log(knownLanguages);
+console.table(knownLanguages);
+
